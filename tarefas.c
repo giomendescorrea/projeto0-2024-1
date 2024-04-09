0476946a9a4962af0c167316f5609e17aec6f0ee
Giovanna Mendes Corrêa <147007498+giomendescorrea@users.noreply.github.com>
2024-04-09 22:44:25 +0000

    definição do tamanho da categoria e da descrição

--- tarefas.c
+++ tarefas.c
@@ -10,10 +10,10 @@ ERROS criar(Tarefa tarefas[], int *pos){
     scanf("%d", &tarefas[*pos].prioridade);
     clearBuffer();
     printf("Entre com a categoria: ");
-    fgets(tarefas[*pos].categoria, 100, stdin);
+    fgets(tarefas[*pos].categoria, CATEGORIA, stdin);
 
     printf("Entre com a descricao: ");
-    fgets(tarefas[*pos].descricao, 300, stdin);
+    fgets(tarefas[*pos].descricao, DESCRICAO, stdin);
 
     *pos = *pos + 1;
 
--- tarefas.h
+++ tarefas.h
@@ -1,9 +1,11 @@
 #define TOTAL 100
+#define DESCRICAO 300
+#define CATEGORIA 100
 
 typedef struct {
     int prioridade;
-    char descricao[300];
-    char categoria[100];
+    char descricao[DESCRICAO];
+    char categoria[CATEGORIA];
 } Tarefa;
 
 typedef enum {OK, MAX_TAREFA, SEM_TAREFAS, NAO_ENCONTRADO, ABRIR, FECHAR, ESCREVER, LER} ERROS;
